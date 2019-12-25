f4fb67cfdc53e9647e927d18e7767b52b00a2f1c
Prathamesh Kulkarni <pk58@buffalo.edu>
2019-12-25 10:25:55 +0530

    Fixed optimal number of moves

--- script.py
+++ script.py
@@ -20,7 +20,7 @@ while num_disks < 3:
 for i in range(num_disks,0,-1):
   left_stack.push(i)
   
-num_optimal_moves = 2**(num_disks-1)
+num_optimal_moves = 2**num_disks-1
 print("\nThe fastest you can solve this game is in {0} moves".format(num_optimal_moves))
 #Get User Input
 def get_input():
